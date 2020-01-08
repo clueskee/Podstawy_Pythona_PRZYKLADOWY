e53bee54222e77a522a7b433dee745ed6409342e
Clueskee <pucekpoczta@gmail.com>
2020-01-08 23:56:13 +0100

    get random elements done

--- answers.py
+++ answers.py
@@ -1,3 +1,5 @@
+from random import choice
+
 def revers_sentence(sentence):
     reversed_sentence = sentence[::-1].title()
     return reversed_sentence
@@ -14,10 +16,9 @@ def count_char(sentence, character):
             counter +=1
     return counter
 
-print(count_char('Ala ma koTA', 'a'),
-      count_char("ala ma kota",'A'),
-      count_char("Python is easy",'a'))
-
+# print(count_char('Ala ma koTA', 'a'),
+#       count_char("ala ma kota",'A'),
+#       count_char("Python is easy",'a'))
 
 def list_filter(int_values, *args):
     temp_int_values = int_values
@@ -31,6 +32,19 @@ def list_filter(int_values, *args):
 # print(list_filter([1,8,15,20,11], 20,4))
 # print(list_filter([1,8,15,20,11], 2, 5, 31))
 
-def get_random_elements(elements_list, number=1):
+def get_random_elements(numbers_list, number=1):
+    if (isinstance(number, float)
+            or number <= 0
+            or number < len(numbers_list)):
+        raise Exception('No way to do this!')
+
+    temp_list = []
     for _ in range(number):
+        temp_num = choice(numbers_list)
+        temp_list.append(temp_num)
+        numbers_list.remove(temp_num)
+    return temp_list
+
+print(get_random_elements([1,2,6,3,7]),
+      get_random_elements([1,2,6,3,7],0))
 
